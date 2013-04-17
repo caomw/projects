f21f2a3a344730401046016b02054616e50abe52
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-04-17 11:32:09 -0700

    minor

--- conv2/run_dem2.sh
+++
@@ -1,7 +0,0 @@
-export LOCAL=1
-stereo WV01_11JAN131652222-P1BS-10200100104A0300_sub2.tif WV01_11JAN131653180-P1BS-1020010011862E00_sub2.tif WV01_11JAN131652222-P1BS-10200100104A0300_sub2.xml WV01_11JAN131653180-P1BS-1020010011862E00_sub2.xml res_sub25/res --corr-seed-mode 2 --disparity-estimation-dem krigged_dem_nsidc_ndv0_fill.tif --disparity-estimation-dem-accuracy 100 --alignment-method none --session-type dg --stereo-file stereo.default --corr-max-levels 2 --threads 16
-
-
-
-
-
