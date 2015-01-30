71ead9ad90ee94e0dcdf407a572240c7dc42d7ac
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2015-01-30 10:28:56 -0800

    Remove some more whitespace

--- eigen/Kabsch.cpp
+++ eigen/Kabsch.cpp
@@ -27,7 +27,6 @@ Eigen::Affine3d Find3DAffineTransform(Eigen::Matrix3Xd in, Eigen::Matrix3Xd out)
   }
   if (dist_in <= 0 || dist_out <= 0)
     return A;
-
   double scale = dist_out/dist_in;
   out /= scale;
 
